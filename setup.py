e43705885f74475829051f5291c2caaf09bc69ef
Syrus Akbary <me@syrusakbary.com>
2016-09-21 00:10:06 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='graphene-sqlalchemy',
-    version='1.0.dev20160917000001',
+    version='1.0.dev20160920000001',
 
     description='Graphene SQLAlchemy integration',
     long_description=open('README.rst').read(),
