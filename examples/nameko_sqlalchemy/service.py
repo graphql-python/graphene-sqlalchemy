8d04e53fc90d9e4397dea930c1ab3aab3cd1e45f
Seth Kigen <kigen@outlook.com>
2018-04-30 19:17:27 +0300

    Added nameko service with http entry point.

---
+++ examples/nameko_sqlalchemy/service.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from nameko.web.handlers import http
+from app import App 
+
+
+class DepartmentService:
+    name = 'department'    
+
+    @http('POST', '/graphql')
+    def query(self, request):
+        return App().query(request)
