e66a0a5b026b37415e40e52f63b58a0085124ea4
Florian Briand <florian@getluko.com>
2019-03-28 20:31:09 +0100

    Add a rename_fields example with Reflected model

--- graphene_sqlalchemy/tests/test_reflected.py
+++ graphene_sqlalchemy/tests/test_reflected.py
@@ -19,3 +19,15 @@ def test_objecttype_registered():
     assert Reflected._meta.model == ReflectedEditor
     assert list(Reflected._meta.fields) == ["editor_id", "name"]
 
+
+class ReflectedWithFieldRenamed(SQLAlchemyObjectType):
+    class Meta:
+        model = ReflectedEditor
+        registry = registry
+        rename_fields = {
+            "name": "editor_name",
+        }
+
+
+def test_objecttype_registered_with_rename_fields():
+    assert list(ReflectedWithFieldRenamed._meta.fields) == ["editor_id", "editor_name"]
