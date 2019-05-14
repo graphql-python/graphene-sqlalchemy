eda4606f89f6948c691161320cbf3d7be69e5b8c
Rafik Belkadi <Ordana1234@gmail.com>
2019-05-14 13:00:07 +0100

    Update __init__.py

--- graphene_sqlalchemy/__init__.py
+++ graphene_sqlalchemy/__init__.py
@@ -1,4 +1,4 @@
-from .types import SQLAlchemyObjectType
+from .types import SQLAlchemyObjectType, SQLAlchemyInputObjectType
 from .fields import SQLAlchemyConnectionField
 from .utils import get_query, get_session
 
@@ -7,6 +7,7 @@ __version__ = "2.1.2"
 __all__ = [
     "__version__",
     "SQLAlchemyObjectType",
+    "SQLAlchemyInputObjectType",
     "SQLAlchemyConnectionField",
     "get_query",
     "get_session",
