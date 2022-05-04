f57efdfab5378ccdd8f8298d31cf3671a7ee9b85
Bryan Malyn <bim9262@gmail.com>
2022-05-04 02:11:57 -0500

    Pick up the docstrings of hybrid properties

--- graphene_sqlalchemy/tests/test_types.py
+++ graphene_sqlalchemy/tests/test_types.py
@@ -85,6 +85,7 @@ def test_sqlalchemy_default_fields():
         # Composite
         "composite_prop",
         # Hybrid
+        "hybrid_prop_with_doc",
         "hybrid_prop",
         "hybrid_prop_str",
         "hybrid_prop_int",
@@ -150,6 +151,12 @@ def test_sqlalchemy_default_fields():
     # "doc" is ignored by hybrid_property
     assert hybrid_prop_list.description is None
 
+    # hybrid_prop_with_doc
+    hybrid_prop_with_doc = ReporterType._meta.fields['hybrid_prop_with_doc']
+    assert hybrid_prop_with_doc.type == String
+    # docstring is picked up from hybrid_prop_with_doc
+    assert hybrid_prop_with_doc.description == "Docstring test"
+
     # relationship
     favorite_article_field = ReporterType._meta.fields['favorite_article']
     assert isinstance(favorite_article_field, Dynamic)
@@ -183,6 +190,7 @@ def test_sqlalchemy_override_fields():
         composite_prop = ORMField()
 
         # hybrid_property
+        hybrid_prop_with_doc = ORMField(description='Overridden')
         hybrid_prop = ORMField(description='Overridden')
 
         # relationships
@@ -210,6 +218,7 @@ def test_sqlalchemy_override_fields():
         "email_v2",
         "column_prop",
         "composite_prop",
+        "hybrid_prop_with_doc",
         "hybrid_prop",
         "favorite_article",
         "articles",
@@ -249,6 +258,11 @@ def test_sqlalchemy_override_fields():
     assert hybrid_prop_field.type == String
     assert hybrid_prop_field.description == "Overridden"
     assert hybrid_prop_field.deprecation_reason is None
+
+    hybrid_prop_with_doc_field = ReporterType._meta.fields['hybrid_prop_with_doc']
+    assert hybrid_prop_with_doc_field.type == String
+    assert hybrid_prop_with_doc_field.description == "Overridden"
+    assert hybrid_prop_with_doc_field.deprecation_reason is None
 
     column_prop_field_v2 = ReporterType._meta.fields['column_prop']
     assert column_prop_field_v2.type == String
@@ -318,6 +332,7 @@ def test_exclude_fields():
         "email",
         "favorite_pet_kind",
         "composite_prop",
+        "hybrid_prop_with_doc",
         "hybrid_prop",
         "hybrid_prop_str",
         "hybrid_prop_int",
@@ -432,7 +447,7 @@ def test_custom_objecttype_registered():
 
     assert issubclass(CustomReporterType, ObjectType)
     assert CustomReporterType._meta.model == Reporter
-    assert len(CustomReporterType._meta.fields) == 16
+    assert len(CustomReporterType._meta.fields) == 17
 
 
 # Test Custom SQLAlchemyObjectType with Custom Options
