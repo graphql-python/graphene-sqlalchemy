968e5e5b97cc60a9d42b5a23e29128ffdf3cb293
Erik Wrede <erikwrede2@gmail.com>
2022-05-26 15:04:50 +0200

    Added Python 3.10; set 3.10 as default version & updated pre-commit hooks

--- setup.py
+++ setup.py
@@ -44,6 +44,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
     keywords="api graphql protocol rest relay graphene",
