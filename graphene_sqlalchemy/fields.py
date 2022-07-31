6417061998823fb3531def8fee394bd81c6f4c24
Paul Schweizer <paulschweizer@gmx.net>
2022-07-31 17:41:50 +0200

    Deprecate UnsortedSQLAlchemyConnectionField and resetting RelationshipLoader between queries

--- graphene_sqlalchemy/fields.py
+++ graphene_sqlalchemy/fields.py
@@ -14,7 +14,7 @@ from .batching import get_batch_resolver
 from .utils import EnumValue, get_query
 
 
-class UnsortedSQLAlchemyConnectionField(ConnectionField):
+class SQLAlchemyConnectionField(ConnectionField):
     @property
     def type(self):
         from .types import SQLAlchemyObjectType
@@ -37,13 +37,45 @@ class UnsortedSQLAlchemyConnectionField(ConnectionField):
         )
         return nullable_type.connection
 
+    def __init__(self, type_, *args, **kwargs):
+        nullable_type = get_nullable_type(type_)
+        if "sort" not in kwargs and nullable_type and issubclass(nullable_type, Connection):
+            # Let super class raise if type is not a Connection
+            try:
+                kwargs.setdefault("sort", nullable_type.Edge.node._type.sort_argument())
+            except (AttributeError, TypeError):
+                raise TypeError(
+                    'Cannot create sort argument for {}. A model is required. Set the "sort" argument'
+                    " to None to disabling the creation of the sort query argument".format(
+                        nullable_type.__name__
+                    )
+                )
+        elif "sort" in kwargs and kwargs["sort"] is None:
+            del kwargs["sort"]
+        super(SQLAlchemyConnectionField, self).__init__(type_, *args, **kwargs)
+
     @property
     def model(self):
         return get_nullable_type(self.type)._meta.node._meta.model
 
     @classmethod
-    def get_query(cls, model, info, **args):
-        return get_query(model, info.context)
+    def get_query(cls, model, info, sort=None, **args):
+        query = get_query(model, info.context)
+        if sort is not None:
+            if not isinstance(sort, list):
+                sort = [sort]
+            sort_args = []
+            # ensure consistent handling of graphene Enums, enum values and
+            # plain strings
+            for item in sort:
+                if isinstance(item, enum.Enum):
+                    sort_args.append(item.value.value)
+                elif isinstance(item, EnumValue):
+                    sort_args.append(item.value)
+                else:
+                    sort_args.append(item)
+            query = query.order_by(*sort_args)
+        return query
 
     @classmethod
     def resolve_connection(cls, connection_type, model, info, args, resolved):
@@ -90,43 +122,16 @@ class UnsortedSQLAlchemyConnectionField(ConnectionField):
         )
 
 
-# TODO Rename this to SortableSQLAlchemyConnectionField
-class SQLAlchemyConnectionField(UnsortedSQLAlchemyConnectionField):
+# TODO Remove in next major version
+class UnsortedSQLAlchemyConnectionField(SQLAlchemyConnectionField):
     def __init__(self, type_, *args, **kwargs):
-        nullable_type = get_nullable_type(type_)
-        if "sort" not in kwargs and issubclass(nullable_type, Connection):
-            # Let super class raise if type is not a Connection
-            try:
-                kwargs.setdefault("sort", nullable_type.Edge.node._type.sort_argument())
-            except (AttributeError, TypeError):
-                raise TypeError(
-                    'Cannot create sort argument for {}. A model is required. Set the "sort" argument'
-                    " to None to disabling the creation of the sort query argument".format(
-                        nullable_type.__name__
-                    )
-                )
-        elif "sort" in kwargs and kwargs["sort"] is None:
-            del kwargs["sort"]
-        super(SQLAlchemyConnectionField, self).__init__(type_, *args, **kwargs)
-
-    @classmethod
-    def get_query(cls, model, info, sort=None, **args):
-        query = get_query(model, info.context)
-        if sort is not None:
-            if not isinstance(sort, list):
-                sort = [sort]
-            sort_args = []
-            # ensure consistent handling of graphene Enums, enum values and
-            # plain strings
-            for item in sort:
-                if isinstance(item, enum.Enum):
-                    sort_args.append(item.value.value)
-                elif isinstance(item, EnumValue):
-                    sort_args.append(item.value)
-                else:
-                    sort_args.append(item)
-            query = query.order_by(*sort_args)
-        return query
+        super(UnsortedSQLAlchemyConnectionField, self).__init__(type_, *args, **kwargs)
+        warnings.warn(
+            "UnsortedSQLAlchemyConnectionField is deprecated and will be removed in the next "
+            "major version. Use SQLAlchemyConnectionField instead and set `sort = None` "
+            "if you want to disable sorting.",
+            DeprecationWarning,
+        )
 
 
 class BatchSQLAlchemyConnectionField(SQLAlchemyConnectionField):
