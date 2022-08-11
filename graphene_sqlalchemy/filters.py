63192783329dafb44d02f54aa474af828c7f7063
Erik Wrede <erikwrede2@gmail.com>
2022-08-11 19:55:06 +0200

    Drafted abstract filters, generation of filter fields from methods.
    Signed-off-by: Erik Wrede <erikwrede2@gmail.com>

--- graphene_sqlalchemy/filters.py
+++ graphene_sqlalchemy/filters.py
@@ -1,4 +1,7 @@
+import re
+
 import graphene
+from graphene.types.inputobjecttype import InputObjectTypeOptions
 
 
 class ObjectTypeFilter(graphene.InputObjectType):
@@ -6,6 +9,11 @@ class ObjectTypeFilter(graphene.InputObjectType):
 
 
 class RelationshipFilter(graphene.InputObjectType):
+    pass
+
+
+class AbstractType:
+    """Dummy class for generic filters"""
     pass
 
 
@@ -18,10 +26,53 @@ class ScalarFilter(graphene.InputObjectType):
     @classmethod
     def __init_subclass_with_meta__(cls, type=None, _meta=None, **options):
         print(type)  # The type from the Meta Class
+
+        # get all filter functions
+        filter_function_regex = re.compile(".+_filter$")
+
+        filter_functions = []
+
+        # Search the entire class for functions matching the filter regex
+        for func in dir(cls):
+            func_attr = getattr(cls, func)
+            # Check if attribute is a function
+            if callable(func_attr) and filter_function_regex.match(func):
+                # add function and attribute name to the list
+                filter_functions.append((func.removesuffix("_filter"), func_attr.__annotations__))
+
+        # Init meta options class if it doesn't exist already
+        if not _meta:
+            _meta = InputObjectTypeOptions(cls)
+
+        new_filter_fields = {}
+
+        # Generate Graphene Fields from the filter functions based on type hints
+        for field_name, annotations in filter_functions:
+            assert "val" in annotations, "Each filter method must have a value field with valid type annotations"
+            # If type is generic, replace with actual type of filter class
+            if annotations["val"] == AbstractType:
+                # TODO Maybe there is an existing class or a more elegant way to solve this
+                # One option would be to only annotate non-abstract filters
+                new_filter_fields.update({field_name: graphene.InputField(type)})
+            else:
+                # TODO this is a place holder, we need to convert the type of val to a valid graphene
+                # type that we can pass to the InputField. We could re-use converter.convert_hybrid_property_return_type
+                new_filter_fields.update({field_name: graphene.InputField(graphene.String)})
+
+        # Add all fields to the meta options. graphene.InputbjectType will take care of the rest
+        if _meta.fields:
+            _meta.fields.update(new_filter_fields)
+        else:
+            _meta.fields = new_filter_fields
+
+        # Pass modified meta to the super class
         super(ScalarFilter, cls).__init_subclass_with_meta__(_meta=_meta, **options)
 
-    # TODO: Make this dynamic based on Meta.Type (see FloatFilter)
-    eq = graphene.Dynamic(None)
+    # Abstract methods can be marked using AbstractType. See comment on the init method
+    @classmethod
+    def eq_filter(cls, val: AbstractType) -> bool:
+        # TBD filtering magic
+        pass
 
 
 class StringFilter(ScalarFilter):
@@ -31,11 +82,18 @@ class StringFilter(ScalarFilter):
 
 class NumberFilter(ScalarFilter):
     """Intermediate Filter class since all Numbers are in an order relationship (support <, > etc)"""
-    pass
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def gt_filter(cls, val: str) -> bool:
+        # TBD filtering magic
+        pass
 
 
 class FloatFilter(NumberFilter):
-    """Cooncrete Filter Class which specifies a type for all the abstract filter methods defined in the super classes"""
+    """Concrete Filter Class which specifies a type for all the abstract filter methods defined in the super classes"""
 
     class Meta:
         type = graphene.Float
