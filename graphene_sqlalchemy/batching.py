0a765a1a0324f0c48e55ae2f0264dc95f094bc1b
Cadu <cadu.coelho@gmail.com>
2022-09-13 04:22:08 -0300

    Made Relationshiploader utilize the new and improved DataLoader implementation housed inside graphene, if possible (graphene >=3.1.1) (#362)

--- graphene_sqlalchemy/batching.py
+++ graphene_sqlalchemy/batching.py
@@ -2,7 +2,6 @@
 from asyncio import get_event_loop
 from typing import Any, Dict
 
-import aiodataloader
 import sqlalchemy
 from sqlalchemy.orm import Session, strategies
 from sqlalchemy.orm.query import QueryContext
@@ -10,7 +9,21 @@ from sqlalchemy.orm.query import QueryContext
 from .utils import is_graphene_version_less_than, is_sqlalchemy_version_less_than
 
 
-class RelationshipLoader(aiodataloader.DataLoader):
+def get_data_loader_impl() -> Any:  # pragma: no cover
+    """Graphene >= 3.1.1 ships a copy of aiodataloader with minor fixes. To preserve backward-compatibility,
+    aiodataloader is used in conjunction with older versions of graphene"""
+    if is_graphene_version_less_than("3.1.1"):
+        from aiodataloader import DataLoader
+    else:
+        from graphene.utils.dataloader import DataLoader
+
+    return DataLoader
+
+
+DataLoader = get_data_loader_impl()
+
+
+class RelationshipLoader(DataLoader):
     cache = False
 
     def __init__(self, relationship_prop, selectin_loader):
@@ -90,20 +103,6 @@ class RelationshipLoader(aiodataloader.DataLoader):
 RELATIONSHIP_LOADERS_CACHE: Dict[
     sqlalchemy.orm.relationships.RelationshipProperty, RelationshipLoader
 ] = {}
-
-
-def get_data_loader_impl() -> Any:  # pragma: no cover
-    """Graphene >= 3.1.1 ships a copy of aiodataloader with minor fixes. To preserve backward-compatibility,
-    aiodataloader is used in conjunction with older versions of graphene"""
-    if is_graphene_version_less_than("3.1.1"):
-        from aiodataloader import DataLoader
-    else:
-        from graphene.utils.dataloader import DataLoader
-
-    return DataLoader
-
-
-DataLoader = get_data_loader_impl()
 
 
 def get_batch_resolver(relationship_prop):
